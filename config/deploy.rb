2b3e016f488118a087e9343a254a8c624873d5ac
Falko Schmidt <scf@panter.ch>
2013-10-11 20:07:46 +0200

    Run `rake appcache` after assets are precompiled.

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,8 +10,8 @@ set :deploy_to, '/var/lib/rails/essence'
 set :scm, :git
 set :repository, 'git@github.com:kaethorn/essence.git'
 set :deploy_via, :remote_cache
-set :default_run_options, { :pty => true }
-set :ssh_options, {:forward_agent => true}
+set :default_run_options, { pty: true }
+set :ssh_options, { forward_agent: true }
 
 set :user, 'passenger'
 set :use_sudo, false
@@ -21,13 +21,18 @@ after 'deploy:update', 'deploy:cleanup'
 
 role :web, 'alphagemini.org'                          # Your HTTP server, Apache/etc
 role :app, 'alphagemini.org'                          # This may be the same as your `Web` server
-#role :db,  'alphagemini.org', :primary => true        # This is where Rails migrations will run
+
+# Generate application.manifest
+task :create_appcache, :roles => [:app] do
+  run 'rake appcache'
+end
+before 'deploy:assets:precompile', :create_appcache
 
 # If you are using Passenger mod_rails uncomment this:
 namespace :deploy do
   task :start do ; end
   task :stop do ; end
-  task :restart, :roles => :app, :except => { :no_release => true } do
+  task :restart, roles: :app, :except => { no_release: true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
 end
